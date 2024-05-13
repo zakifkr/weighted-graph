a1767b02e46c99fb17769639ed6a5abda8dc616c
YosionBestyMarpaung <147596895+YosionBestyMarpaung@users.noreply.github.com>
2024-05-13 12:04:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,3 +19,28 @@ def dijkstra(graph, start, end):
         #Jika jarak saat ini lebih besar dari jarak terpendek yang sudah diketahui ke simpul saat ini, maka simpul ini diabaikan.
         if current_distance > distances[current_node]:
             continue
+        
+        #Untuk setiap tetangga dari simpul saat ini, menghitung jarak baru ke tetangga tersebut. Jika jarak baru ini lebih pendek dari jarak terpendek yang sudah diketahui ke tetangga tersebut, maka jarak terpendek ke tetangga tersebut diperbarui dan tetangga tersebut ditambahkan ke antrian dengan jarak baru tersebut.
+        for neighbor, weight in graph[current_node].items():
+            distance = current_distance + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
+                heapq.heappush(queue, (distance, neighbor))
+            
+
+    return distances[end]
+
+# Contoh graf berbobot yang merepresentasikan keterhubungan antar kota di Jawa Tengah.
+graf = {
+    'Brebes' : {'Tegal': 50, 'Slawi': 30},
+    'Tegal' : {'Brebes': 50, 'Slawi': 60, 'Pemalang': 40},
+    'Slawi' : {'Brebes': 30, 'Tegal': 60, 'Purwokerto': 70},
+    'Purwokerto' : {'Slawi': 70, 'Cilacap': 80, 'Kroya': 50, 'Kebumen': 60, 'Purbalingga': 40},
+    'Pemalang' : {'Tegal': 40, 'Purbalingga': 90, 'Pekalongan': 30},
+    'Purbalingga' : {'Purwokerto': 40, 'Pemalang': 90, 'Banjarnegara': 70},
+    'Pekalongan' : {'Pemalang': 30, 'Kendal': 20},
+    'Cilacap' : {'Purwokerto': 80, 'Kroya': 50},
+    'Kroya' : {'Cilacap': 50, 'Purwokerto': 50, 'Kebumen': 40},
+    'Kebumen' : {'Purwokerto': 60, 'Kroya': 40, 'Purworejo': 30}
+
+
